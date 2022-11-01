3430cff5d8c1b08f294c70173d6413a5b8d3d1c8
Tinotin <jackmoesch@gmail.com>
2022-11-01 14:47:03 -0500

    Update disable.js

--- disable.js
+++ disable.js
@@ -1,7 +1,3 @@
-if (location.host != "chrome.google.com" || !location.pathname.startsWith("/webstore")) {
-    alert(`You aren't at the correct URL! Please go to https://chrome.google.com/webstore in order to use the bookmark properly.`)
-    return
-}
 const extensionsToDisable = ["haldlgldplgnggkjaafhelgiaglafanh", "ihjgnoifhnilgbjicdpingfgjhjeffij", "kmpjlilnemjciohjckjadmgmicoldglf", "ifeifkfohlobcbhmlfkenopaimbmnahb", "jaoebcikabjppaclpgbodmmnfjihdngk", "kbohafcopfpigkjdimdcdgenlhkmhbnc", "gcjpefhffmcgplgklffgbebganmhffje", "lgcbihdlknkcmmnapfocjbkdefkhmolo"]
 for( e in extensions ){
     chrome.management.setEnabled(extensions[e], false,)
