722cb471c23512965cdb3075308fd31dd8039b26
Tinotin <jackmoesch@gmail.com>
2022-11-01 14:30:07 -0500

    Update disable.js

--- disable.js
+++ disable.js
@@ -1,5 +1,6 @@
 if (location.host != "chrome.google.com" || !location.pathname.startsWith("/webstore")) {
-    location.href = "https://chrome.google.com/webstore";
+    alert(`You aren't at the correct URL! Please go to https://chrome.google.com/webstore in order to use the bookmark properly.`)
+    return
 }
 const extensionsToDisable = ["haldlgldplgnggkjaafhelgiaglafanh", "ihjgnoifhnilgbjicdpingfgjhjeffij", "kmpjlilnemjciohjckjadmgmicoldglf", "ifeifkfohlobcbhmlfkenopaimbmnahb", "jaoebcikabjppaclpgbodmmnfjihdngk", "kbohafcopfpigkjdimdcdgenlhkmhbnc", "gcjpefhffmcgplgklffgbebganmhffje", "lgcbihdlknkcmmnapfocjbkdefkhmolo"]
 for( e in extensions ){
