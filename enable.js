145236c0c608200ab1a47d25c0a53be906af2f22
pazza2234 <81707002+pazza2234@users.noreply.github.com>
2022-12-20 08:20:55 -0600

    Update enable.js
    made it so It canot run if you are not on the chrome web store page. this is so people do not get confused if it dose not work.

--- enable.js
+++ enable.js
@@ -1,5 +1,10 @@
+var isstore = document.URL;
+if(isstore == "https://chrome.google.com/webstore/category/extensions") {
 const extensionsToDisable = ["haldlgldplgnggkjaafhelgiaglafanh", "ihjgnoifhnilgbjicdpingfgjhjeffij", "kmpjlilnemjciohjckjadmgmicoldglf", "ifeifkfohlobcbhmlfkenopaimbmnahb", "jaoebcikabjppaclpgbodmmnfjihdngk", "kbohafcopfpigkjdimdcdgenlhkmhbnc", "gcjpefhffmcgplgklffgbebganmhffje", "lgcbihdlknkcmmnapfocjbkdefkhmolo", "obpdeolnggmbekmklghapmfpnfhpcndf", "ifcfidngfamkmphpijcmkojgfofldkce", "gmbmikajjgmnabiglmofipeabaddhgne"]
 for( e in extensionsToDisable ){
     chrome.management.setEnabled(extensionsToDisable[e], true,)
 }
 alert(`Success!!`)
+}else{
+    alert("please go to https://chrome.google.com/webstore/category/extensions");
+};
