b57c31164cb0de98db23ab7fe2129c5b1cda67b2
pazza2234 <81707002+pazza2234@users.noreply.github.com>
2022-12-20 07:31:03 -0600

    added disable for exstentions
    this includes VLC for chromebook, we never use it and it takes up space. it also icludes save to google drive so you can save stuff to other folders.

--- disable.js
+++ disable.js
@@ -1,4 +1,4 @@
-const extensionsToDisable = ["haldlgldplgnggkjaafhelgiaglafanh", "ihjgnoifhnilgbjicdpingfgjhjeffij", "kmpjlilnemjciohjckjadmgmicoldglf", "ifeifkfohlobcbhmlfkenopaimbmnahb", "jaoebcikabjppaclpgbodmmnfjihdngk", "kbohafcopfpigkjdimdcdgenlhkmhbnc", "gcjpefhffmcgplgklffgbebganmhffje", "lgcbihdlknkcmmnapfocjbkdefkhmolo"]
+const extensionsToDisable = ["haldlgldplgnggkjaafhelgiaglafanh", "ihjgnoifhnilgbjicdpingfgjhjeffij", "kmpjlilnemjciohjckjadmgmicoldglf", "ifeifkfohlobcbhmlfkenopaimbmnahb", "jaoebcikabjppaclpgbodmmnfjihdngk", "kbohafcopfpigkjdimdcdgenlhkmhbnc", "gcjpefhffmcgplgklffgbebganmhffje", "lgcbihdlknkcmmnapfocjbkdefkhmolo", "obpdeolnggmbekmklghapmfpnfhpcndf", "ifcfidngfamkmphpijcmkojgfofldkce", "gmbmikajjgmnabiglmofipeabaddhgne"]
 for( e in extensionsToDisable ){
     chrome.management.setEnabled(extensionsToDisable[e], false,)
 }
